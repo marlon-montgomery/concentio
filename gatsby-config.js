a406487395106ac753ff9419b0aa4142ec39bc11
Marlon Montgomery <marlon.montgomery@globant.com>
2019-11-14 21:15:02 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `Concentio`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,6 +13,16 @@ module.exports = {
       options: {
         apiToken: process.env.DATO_API_TOKEN,
       },
+      {
+       resolve: 'gatsby-remark-audio',
+       options: {
+          preload: 'auto',
+          loop: false,
+          controls: true,
+          muted: false,
+          autoplay: false
+        },      
+      {   
     },
   ],
 }
